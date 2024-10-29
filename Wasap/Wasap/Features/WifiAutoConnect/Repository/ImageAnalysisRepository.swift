4f61eab4fc2b5ff6604a6dcd24f59a190a74271c
Jonghyeon Chang <hokhmah91@naver.com>
2024-10-29 17:08:07 +0900

    [#51] completionHandler 밖으로 이동

--- Wasap/Wasap/Features/WifiAutoConnect/Repository/ImageAnalysisRepository.swift
+++ Wasap/Wasap/Features/WifiAutoConnect/Repository/ImageAnalysisRepository.swift
@@ -29,150 +29,20 @@ public class DefaultImageAnalysisRepository: ImageAnalysisRepository {
                 single(.failure(ImageAnalysisError.ocrFailed("Self not found")))
                 return Disposables.create()
             }
-            
+
+            let request = VNRecognizeTextRequest { [weak self] request, error in
+                self?.recognizeTextHandler(request: request, error: error, single: single)
+            }
+
             guard let cgImage = self.convertDataToCGImage(imageData) else {
                 single(.failure(ImageAnalysisError.invalidImage))
                 return Disposables.create()
             }
-            
             let orientation = self.extractOrientation(from: imageData)
-            
+
             let requestHandler = VNImageRequestHandler(cgImage: cgImage, orientation: orientation, options: [:])
 
-            let request = VNRecognizeTextRequest { request, error in
-                if let error = error {
-                    single(.failure(ImageAnalysisError.ocrFailed(error.localizedDescription)))
-                    return
-                }
-                
-                guard let results = request.results as? [VNRecognizedTextObservation] else {
-                    single(.failure(ImageAnalysisError.ocrFailed("No results found")))
-                    return
-                }
-                
-                var boxes: [(CGRect, String)] = []
-                var idBoxes: [CGRect] = []
-                var pwBoxes: [CGRect] = []
-                
-                for observation in results {
-                    print("--------------------------텍스트 분리----------------------------")
-                    if let topCandidate = observation.topCandidates(1).first {
-                        let originalString = topCandidate.string
-                        let boundingBox = observation.boundingBox
-                        
-                        // 1차: 공백 제거
-                        let noSpaceString = originalString.replacingOccurrences(of: " ", with: "")
-                        
-                        // 2차: 콜론(:) 및 하이픈(-) 제거
-                        let cleanedString = noSpaceString.replacingOccurrences(of: "[:\\-]", with: " ", options: .regularExpression)
-                        
-                        // 텍스트가 "ID" 또는 "PW"로 시작하는지 확인
-                        let components = cleanedString.split(separator: " ", maxSplits: 1, omittingEmptySubsequences: true)
-                        guard let firstWord = components.first.map(String.init) else { continue }
-                        
-                        if self.idStrings.contains(firstWord) {
-                            // "ID" 부분을 분리하고 나머지 텍스트와 나눔
-                            let idText = firstWord
-                            let remainingText = components.count > 1 ? String(components[1]) : ""
-                            
-                            // "ID" 부분에 대한 Bounding Box 분리
-                            let idBox = boundingBox
-                            let remainingBox = self.splitBoundingBox(originalBox: boundingBox, splitFactor: CGFloat(1 - Double(remainingText.count)/Double(noSpaceString.count)))
-
-                            idBoxes.append(idBox)
-                            boxes.append((idBox, "ID"))
-                            
-                            if !remainingText.isEmpty {
-                                boxes.append((remainingBox, remainingText))
-                            }
-                            
-                            Log.print("원본텍스트:\(originalString), 분리된텍스트:'\(idText)' + '\(remainingText)'")
-                            
-                        } else if self.pwStrings.contains(firstWord) {
-                            // "PW" 부분을 분리하고 나머지 텍스트와 나눔
-                            let pwText = firstWord
-                            let remainingText = components.count > 1 ? String(components[1]) : ""
-                            
-                            // "PW" 부분에 대한 Bounding Box 분리
-                            let pwBox = boundingBox
-                            let remainingBox = self.splitBoundingBox(originalBox: boundingBox, splitFactor: CGFloat(1 - Double(remainingText.count)/Double(noSpaceString.count)))
-
-                            pwBoxes.append(pwBox)
-                            boxes.append((pwBox, "PW"))
-                            
-                            if !remainingText.isEmpty {
-                                boxes.append((remainingBox, remainingText))
-                            }
-                            
-                            Log.print("원본텍스트:\(originalString), 분리된텍스트:'\(pwText)' + '\(remainingText)'")
-                            
-                        } else {
-                            boxes.append((boundingBox, originalString))
-                            Log.print("원본텍스트:\(originalString), 기타텍스트:\(cleanedString)")
-                        }
-                    }
-                }
-                
-                var finalBoxes: [(CGRect, CGColor)] = boxes.map { ($0.0, CGColor(red: 0, green: 0, blue: 0, alpha: 1)) }
-                var extractedBoxes: [CGRect] = []
-
-                // "ID"에 가장 가까운 Bounding Box(SSID 값, 보라색) 탐색 - PW는 제외
-                for idBox in idBoxes {
-                    print("-----아이디박스----")
-                    if let closestBox = self.closestBoundingBox(from: idBox, in: boxes.filter { $0.1 != "ID" && $0.1 != "PW" }),
-                       let index = boxes.firstIndex(where: { $0.0 == closestBox.0 }) {
-                        finalBoxes[index].1 = CGColor(red: 0.5, green: 0, blue: 0.5, alpha: 1)
-
-                        extractedBoxes.append(closestBox.0)
-                        self.ssidText = closestBox.1.replacingOccurrences(of: " ", with: "")
-                        Log.print("보라색박스(SSID 값 추정):\(self.ssidText)")
-                        
-                        let distance = self.distanceBetweenEdges(idBox, closestBox.0)
-                        print("ID 박스 CGRect: \(self.formatCGRect(idBox))")
-                        print("SSID 박스 CGRect: \(self.formatCGRect(closestBox.0))")
-                        print("ID 박스와 SSID 값의 거리: \(String(format: "%.3f", distance))")
-                    }
-                }
-                
-                // "PW"에 가장 가까운 Bounding Box(Password 값, 연두색) 탐색 - ID와 SSID value 박스는 제외
-                for pwBox in pwBoxes {
-                    print("-----비번박스----")
-                    if let closestBox = self.closestBoundingBox(from: pwBox, in: boxes.filter { box in
-                        box.1 != "ID" && box.1 != "PW" && finalBoxes.first(where: { finalBox in box.0 == finalBox.0 && finalBox.1 == CGColor(red: 0.5, green: 0, blue: 0.5, alpha: 1) }) == nil }),
-                       let index = boxes.firstIndex(where: { $0.0 == closestBox.0 }) {
-                        finalBoxes[index].1 = CGColor(red: 0, green: 1, blue: 0, alpha: 1)
-
-                        extractedBoxes.append(closestBox.0)
-                        self.passwordText = closestBox.1.replacingOccurrences(of: " ", with: "")
-                        Log.print("연두색박스(Password 값 추정):\(self.passwordText)")
-                        
-                        let distance = self.distanceBetweenEdges(pwBox, closestBox.0)
-                        print("PW 박스 CGRect: \(self.formatCGRect(pwBox))")
-                        print("Password 박스 CGRect: \(self.formatCGRect(closestBox.0))")
-                        print("PW 박스와 Password 값의 거리: \(String(format: "%.3f", distance))")
-                    }
-                }
-                
-                // "ID"는 노란색박스, "PW"는 파란색박스로 지정
-                for (index, box) in boxes.enumerated() {
-                    print("-----항목박스----")
-                    if box.1 == "ID" {
-                        finalBoxes[index].1 = CGColor(red: 1, green: 1, blue: 0, alpha: 1)
-                        Log.print("노란색박스(ID 키):\(box.1)")
-                    } else if box.1 == "PW" {
-                        finalBoxes[index].1 = CGColor(red: 0, green: 0, blue: 1, alpha: 1)
-                        Log.print("파란색박스(PW 키):\(box.1)")
-                    }
-                }
-                
-                DispatchQueue.main.async {
-                    self.boundingBoxes.append(contentsOf: extractedBoxes)
-
-                    single(.success((self.boundingBoxes, self.ssidText, self.passwordText)))
-                }
-            }
-            
-            request.recognitionLanguages = ["en"]
+            request.recognitionLanguages = ["ko", "en"]
             request.usesLanguageCorrection = true
             
             do {
@@ -184,7 +54,141 @@ public class DefaultImageAnalysisRepository: ImageAnalysisRepository {
             return Disposables.create()
         }
     }
-    
+
+    private func recognizeTextHandler(request: VNRequest, error: Error?, single: @escaping (SingleEvent<([CGRect], String, String)>) -> Void) {
+        if let error = error {
+            single(.failure(ImageAnalysisError.ocrFailed(error.localizedDescription)))
+            return
+        }
+
+        guard let results = request.results as? [VNRecognizedTextObservation] else {
+            single(.failure(ImageAnalysisError.ocrFailed("No results found")))
+            return
+        }
+
+        var boxes: [(CGRect, String)] = []
+        var idBoxes: [CGRect] = []
+        var pwBoxes: [CGRect] = []
+
+        for observation in results {
+            print("--------------------------텍스트 분리----------------------------")
+            if let topCandidate = observation.topCandidates(1).first {
+                let originalString = topCandidate.string
+                let boundingBox = observation.boundingBox
+
+                // 1차: 공백 제거
+                let noSpaceString = originalString.replacingOccurrences(of: " ", with: "")
+
+                // 2차: 콜론(:) 및 하이픈(-) 제거
+                let cleanedString = noSpaceString.replacingOccurrences(of: "[:\\-]", with: " ", options: .regularExpression)
+
+                // 텍스트가 "ID" 또는 "PW"로 시작하는지 확인
+                let components = cleanedString.split(separator: " ", maxSplits: 1, omittingEmptySubsequences: true)
+                guard let firstWord = components.first.map(String.init) else { continue }
+
+                if self.idStrings.contains(firstWord) {
+                    // "ID" 부분을 분리하고 나머지 텍스트와 나눔
+                    let idText = firstWord
+                    let remainingText = components.count > 1 ? String(components[1]) : ""
+
+                    // "ID" 부분에 대한 Bounding Box 분리
+                    let idBox = boundingBox
+                    let remainingBox = self.splitBoundingBox(originalBox: boundingBox, splitFactor: CGFloat(1 - Double(remainingText.count)/Double(noSpaceString.count)))
+
+                    idBoxes.append(idBox)
+                    boxes.append((idBox, "ID"))
+
+                    if !remainingText.isEmpty {
+                        boxes.append((remainingBox, remainingText))
+                    }
+
+                    Log.print("원본텍스트:\(originalString), 분리된텍스트:'\(idText)' + '\(remainingText)'")
+
+                } else if self.pwStrings.contains(firstWord) {
+                    // "PW" 부분을 분리하고 나머지 텍스트와 나눔
+                    let pwText = firstWord
+                    let remainingText = components.count > 1 ? String(components[1]) : ""
+
+                    // "PW" 부분에 대한 Bounding Box 분리
+                    let pwBox = boundingBox
+                    let remainingBox = self.splitBoundingBox(originalBox: boundingBox, splitFactor: CGFloat(1 - Double(remainingText.count)/Double(noSpaceString.count)))
+
+                    pwBoxes.append(pwBox)
+                    boxes.append((pwBox, "PW"))
+
+                    if !remainingText.isEmpty {
+                        boxes.append((remainingBox, remainingText))
+                    }
+
+                    Log.print("원본텍스트:\(originalString), 분리된텍스트:'\(pwText)' + '\(remainingText)'")
+
+                } else {
+                    boxes.append((boundingBox, originalString))
+                    Log.print("원본텍스트:\(originalString), 기타텍스트:\(cleanedString)")
+                }
+            }
+        }
+
+        var finalBoxes: [(CGRect, CGColor)] = boxes.map { ($0.0, CGColor(red: 0, green: 0, blue: 0, alpha: 1)) }
+        var extractedBoxes: [CGRect] = []
+
+        // "ID"에 가장 가까운 Bounding Box(SSID 값, 보라색) 탐색 - PW는 제외
+        for idBox in idBoxes {
+            print("-----아이디박스----")
+            if let closestBox = self.closestBoundingBox(from: idBox, in: boxes.filter { $0.1 != "ID" && $0.1 != "PW" }),
+               let index = boxes.firstIndex(where: { $0.0 == closestBox.0 }) {
+                finalBoxes[index].1 = CGColor(red: 0.5, green: 0, blue: 0.5, alpha: 1)
+
+                extractedBoxes.append(closestBox.0)
+                self.ssidText = closestBox.1.replacingOccurrences(of: " ", with: "")
+                Log.print("보라색박스(SSID 값 추정):\(self.ssidText)")
+
+                let distance = self.distanceBetweenEdges(idBox, closestBox.0)
+                print("ID 박스 CGRect: \(self.formatCGRect(idBox))")
+                print("SSID 박스 CGRect: \(self.formatCGRect(closestBox.0))")
+                print("ID 박스와 SSID 값의 거리: \(String(format: "%.3f", distance))")
+            }
+        }
+
+        // "PW"에 가장 가까운 Bounding Box(Password 값, 연두색) 탐색 - ID와 SSID value 박스는 제외
+        for pwBox in pwBoxes {
+            print("-----비번박스----")
+            if let closestBox = self.closestBoundingBox(from: pwBox, in: boxes.filter { box in
+                box.1 != "ID" && box.1 != "PW" && finalBoxes.first(where: { finalBox in box.0 == finalBox.0 && finalBox.1 == CGColor(red: 0.5, green: 0, blue: 0.5, alpha: 1) }) == nil }),
+               let index = boxes.firstIndex(where: { $0.0 == closestBox.0 }) {
+                finalBoxes[index].1 = CGColor(red: 0, green: 1, blue: 0, alpha: 1)
+
+                extractedBoxes.append(closestBox.0)
+                self.passwordText = closestBox.1.replacingOccurrences(of: " ", with: "")
+                Log.print("연두색박스(Password 값 추정):\(self.passwordText)")
+
+                let distance = self.distanceBetweenEdges(pwBox, closestBox.0)
+                print("PW 박스 CGRect: \(self.formatCGRect(pwBox))")
+                print("Password 박스 CGRect: \(self.formatCGRect(closestBox.0))")
+                print("PW 박스와 Password 값의 거리: \(String(format: "%.3f", distance))")
+            }
+        }
+
+        // "ID"는 노란색박스, "PW"는 파란색박스로 지정
+        for (index, box) in boxes.enumerated() {
+            print("-----항목박스----")
+            if box.1 == "ID" {
+                finalBoxes[index].1 = CGColor(red: 1, green: 1, blue: 0, alpha: 1)
+                Log.print("노란색박스(ID 키):\(box.1)")
+            } else if box.1 == "PW" {
+                finalBoxes[index].1 = CGColor(red: 0, green: 0, blue: 1, alpha: 1)
+                Log.print("파란색박스(PW 키):\(box.1)")
+            }
+        }
+
+        DispatchQueue.main.async {
+            self.boundingBoxes.append(contentsOf: extractedBoxes)
+
+            single(.success((self.boundingBoxes, self.ssidText, self.passwordText)))
+        }
+    }
+
+
     // CGRect를 소수점 셋째자리까지 포맷팅하는 함수
     private func formatCGRect(_ rect: CGRect) -> String {
         return String(format: "(x: %.3f, y: %.3f, width: %.3f, height: %.3f)", rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
