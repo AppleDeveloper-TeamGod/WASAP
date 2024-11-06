a75fbc4a1f9af00ea90ea4bbf49660d1172a7d13
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-11-06 19:38:26 +0900

    GoogleService-Info.plist 추가

--- Wasap/Wasap/Global/SceneDelegate.swift
+++ Wasap/Wasap/Global/SceneDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     var coordinator: RootCoordinator?
     let appDIConainter = AppDIContainer(apiClient: DefaultAPIClient())
