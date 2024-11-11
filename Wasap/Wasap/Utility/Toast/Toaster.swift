734a3174b949b977fe1c2e786ce0cfb6fd1349f6
chongin12(Mosu) <chongin12@naver.com>
2024-11-11 17:13:12 +0900

    [#61] Toaster 한 번에 하나만 뜨게 하고, 중요한 토스트는 먼저 띄우기

--- Wasap/Wasap/Utility/Toast/Toaster.swift
+++ Wasap/Wasap/Utility/Toast/Toaster.swift
@@ -14,8 +14,45 @@ public final class Toaster {
 
     private weak var connectedViewController: UIViewController?
 
+    private var toastQueue = DispatchQueue(label: "ToastQueue", qos: .background)
+    private var importantToastQueue = DispatchQueue(label: "ImportantToastQueue", qos: .userInitiated)
+    private var toastSemaphore = DispatchSemaphore(value: 1)
+
     public func connect(to connectedViewController: UIViewController) {
         self.connectedViewController = connectedViewController
+    }
+
+    public func importantToast(_ message: String, duration: TimeInterval = 1.0, delay: TimeInterval = 2.0) {
+        guard let connectedViewController else {
+            Log.error("Error! No connected view")
+            return
+        }
+
+        importantToastQueue.async {
+            self.toastSemaphore.wait()
+            let toastTaskItem = DispatchWorkItem {
+                let toastView = ToastView()
+
+                toastView.toastLabel.text = message
+
+                connectedViewController.view.addSubview(toastView)
+
+                toastView.snp.makeConstraints {
+                    $0.top.equalTo(connectedViewController.view.safeAreaLayoutGuide).offset(24)
+                    $0.centerX.equalToSuperview()
+
+                    $0.height.greaterThanOrEqualToSuperview().multipliedBy(0.06)
+                }
+
+                UIView.animate(withDuration: duration, delay: delay) {
+                    toastView.alpha = 0.0
+                } completion: { _ in
+                    toastView.removeFromSuperview()
+                    self.toastSemaphore.signal()
+                }
+            }
+            DispatchQueue.main.async(execute: toastTaskItem)
+        }
     }
 
     public func toast(_ message: String, duration: TimeInterval = 1.0, delay: TimeInterval = 2.0) {
@@ -24,23 +61,35 @@ public final class Toaster {
             return
         }
 
-        let toastView = ToastView()
+        toastQueue.async {
+            switch self.toastSemaphore.wait(timeout: .now() + 10) { // 10초 넘게 기다리면 실행하지 않고 폐기.
+            case .success:
+                break
+            case .timedOut:
+                return
+            }
+            let toastTaskItem = DispatchWorkItem {
+                let toastView = ToastView()
 
-        toastView.toastLabel.text = message
+                toastView.toastLabel.text = message
 
-        connectedViewController.view.addSubview(toastView)
+                connectedViewController.view.addSubview(toastView)
 
-        toastView.snp.makeConstraints {
-            $0.top.equalTo(connectedViewController.view.safeAreaLayoutGuide).offset(24)
-            $0.centerX.equalToSuperview()
+                toastView.snp.makeConstraints {
+                    $0.top.equalTo(connectedViewController.view.safeAreaLayoutGuide).offset(24)
+                    $0.centerX.equalToSuperview()
 
-            $0.height.greaterThanOrEqualToSuperview().multipliedBy(0.06)
-        }
+                    $0.height.greaterThanOrEqualToSuperview().multipliedBy(0.06)
+                }
 
-        UIView.animate(withDuration: duration, delay: delay) {
-            toastView.alpha = 0.0
-        } completion: { _ in
-            toastView.removeFromSuperview()
+                UIView.animate(withDuration: duration, delay: delay) {
+                    toastView.alpha = 0.0
+                } completion: { _ in
+                    toastView.removeFromSuperview()
+                    self.toastSemaphore.signal()
+                }
+            }
+            DispatchQueue.main.async(execute: toastTaskItem)
         }
     }
 }
