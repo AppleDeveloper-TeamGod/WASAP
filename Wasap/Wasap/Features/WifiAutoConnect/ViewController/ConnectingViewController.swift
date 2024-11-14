fcf1e51c71d77ea071707077926699a34dd6f1f3
Jonghyeon Chang <hokhmah91@naver.com>
2024-11-14 16:26:58 +0900

    [#72] 공유하기 버튼에 Fade-in 효과 적용

--- Wasap/Wasap/Features/WifiAutoConnect/ViewController/ConnectingViewController.swift
+++ Wasap/Wasap/Features/WifiAutoConnect/ViewController/ConnectingViewController.swift
@@ -12,25 +12,25 @@ import Lottie
 
 public class ConnectingViewController: RxBaseViewController<ConnectingViewModel> {
     private let connectingView = ConnectingView()
-    
+
     public override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     public override func loadView() {
         super.loadView()
         self.view = connectingView
     }
-    
+
     override init(viewModel: ConnectingViewModel) {
         super.init(viewModel: viewModel)
         bind(viewModel)
     }
-    
+
     @MainActor required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func bind(_ viewModel: ConnectingViewModel) {
         // 뷰 -> 뷰모델
         connectingView.quitButton.rx.tap
@@ -40,7 +40,6 @@ public class ConnectingViewController: RxBaseViewController<ConnectingViewModel>
         connectingView.shareButton.rx.tap
             .bind(to: viewModel.shareButtonTapped)
             .disposed(by: disposeBag)
-
 
         // 뷰모델 -> 뷰
         viewModel.isLoading
@@ -52,7 +51,7 @@ public class ConnectingViewController: RxBaseViewController<ConnectingViewModel>
                 self?.connectingView.shareButton.isHidden = true
             }
             .disposed(by: disposeBag)
-        
+
         viewModel.isWiFiConnected
             .filter { $0 }
             .drive { [weak self] _ in
@@ -64,9 +63,11 @@ public class ConnectingViewController: RxBaseViewController<ConnectingViewModel>
                 self?.connectingView.subStatusLabel.isHidden = false
                 self?.connectingView.quitButton.isHidden = false
 
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
-                    self?.connectingView.shareButton.isHidden = false
-                }
+                self?.connectingView.shareButton.isHidden = false
+                self?.connectingView.shareButton.alpha = 0.0
+                UIView.animate(withDuration: 0.1, delay: 0.55, options: [.curveEaseIn], animations: {
+                    self?.connectingView.shareButton.alpha = 1.0
+                })
             }
             .disposed(by: disposeBag)
     }
