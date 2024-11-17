17e21c542ac8038f119fd3dcedfae875e1752b3b
chongin12(Mosu) <chongin12@naver.com>
2024-11-17 17:28:22 +0900

    [#79] ViewModel과 View간 이벤트 연결

--- Wasap/Wasap/Features/WifiAutoConnect/ViewController/OnboardingViewController.swift
+++ Wasap/Wasap/Features/WifiAutoConnect/ViewController/OnboardingViewController.swift
@@ -10,8 +10,12 @@ import RxSwift
 
 public class OnboardingViewController: RxBaseViewController<OnboardingViewModel> {
     private let onboardingView = OnboardingView()
+    private let onboardingPages: [UIView.Type]
+    private static let contentWidth = UIScreen.main.bounds.width
+    private static let contentHeight = UIScreen.main.bounds.height * 0.55
 
     override public init(viewModel: OnboardingViewModel) {
+        self.onboardingPages = OnboardingViewModel.onboardingPages
         super.init(viewModel: viewModel)
         bind(viewModel)
     }
@@ -27,17 +31,53 @@ public class OnboardingViewController: RxBaseViewController<OnboardingViewModel>
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        onboardingView.setScrollViewContents()
+        setScrollViewContents(self.onboardingPages)
     }
 
     private func bind(_ viewModel: OnboardingViewModel) {
+        self.onboardingView.skipButton.rx.tap
+            .bind(to: viewModel.skipButtonDidTap)
+            .disposed(by: disposeBag)
 
+        self.onboardingView.nextButton.rx.tap
+            .bind(to: viewModel.nextButtonDidTap)
+            .disposed(by: disposeBag)
+
+        self.onboardingView.scrollView.rx.contentOffset
+            .withUnretained(self)
+            .map { owner, offset in
+                let pageIndex = round(offset.x / Self.contentWidth)
+                return Int(pageIndex)
+            }
+            .bind(to: viewModel.currentPageInput)
+            .disposed(by: disposeBag)
+
+        self.onboardingView.pageControl.rx.controlEvent(.valueChanged)
+            .compactMap { [weak self] _ in
+                self?.onboardingView.pageControl.currentPage
+            }
+            .bind(to: viewModel.currentPageInput)
+            .disposed(by: disposeBag)
+
+        viewModel.currentPageOutput
+            .drive { [weak self] pageNumber in
+                self?.onboardingView.pageControl.currentPage = pageNumber
+                self?.onboardingView.scrollView.contentOffset.x = CGFloat(pageNumber) * Self.contentWidth
+            }
+            .disposed(by: disposeBag)
     }
-}
 
-extension OnboardingViewController: UIScrollViewDelegate {
-    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
-        onboardingView.pageControl.currentPage = Int(pageIndex)
+    private func setScrollViewContents(_ onboardingPages: [UIView.Type]) {
+        let contentWidth = Self.contentWidth
+        let contentHeight = Self.contentHeight
+
+        self.onboardingView.pageControl.numberOfPages = onboardingPages.count
+
+        self.onboardingView.scrollView.contentSize = CGSize(width: contentWidth * CGFloat(onboardingPages.count), height: contentHeight)
+        onboardingPages.enumerated().forEach { index, page in
+            let rect = CGRect(x: contentWidth * CGFloat(index), y: 0, width: contentWidth, height: contentHeight)
+            let view = page.init(frame: rect)
+            self.onboardingView.scrollView.addSubview(view)
+        }
     }
 }
